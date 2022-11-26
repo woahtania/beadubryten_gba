0f557302b45e62eca8293a710569e9ef5ced32e8
Tania <90781871+woahtania@users.noreply.github.com>
2022-11-26 01:32:53 +0000

    Adds battle backend code

--- include/battle.h
+++ include/battle.h
@@ -18,24 +18,95 @@
 #define BUFF_STRENGTH 1
 #define BUFF_SPEED 2
 
-struct Tile{
-    int team; // one of the defined teams above or -1 for generic
-    int type; // the type of tile (land, water, or air)
+#define MAP_W 32
+#define MAP_H 32
+
+#define MAX_UNITS 10
+
+/**
+ * Data form for a Tile type
+ */
+struct Tile
+{
+    int team;     // one of the defined teams above or -1 for generic
+    int type;     // the type of tile (land, water, or air)
     int buffType; // the type of buff applied to home team and debuff applied to home team
     int buffStrength = 1;
-    char* name;
+    char *name;
 };
 
-struct Unit{
-    char* name;
+/**
+ * Data form for a Unit type
+ */
+struct Unit
+{
+    char *name;
     int team;
     int type;
     int stats[3]; // stats
     bool isSignatureUnit;
+    int maxHealth;
 };
 
+/**
+ * Map Unit (loaded version of Unit)
+ */
+struct MUnit
+{
+    int type;
+    int health;
+    int movement;
+    bool hasAttackedThisTurn;
+    bool isVisibleThisTurn;
+    int x, y;
+};
+
+/**
+ * Every unit's static data
+ */
 extern const struct Unit allUnits[12];
 
+/**
+ * Every tile's static data
+ */
 extern const struct Tile allTiles[12];
+
+/**
+ * The current map's tiles
+ */
+extern const int mapTiles[MAP_W * MAP_H];
+
+/**
+ * The current map's visible tiles
+ */
+extern bool visibleMapTiles[MAP_W * MAP_H];
+
+/**
+ * The currently loaded units
+ */
+extern struct MUnit loadedUnits[MAX_UNITS * 3];
+
+/**
+ * Begins the turn for a particular team, setting all loaded units' values
+ * @param team the team to start for
+ */
+void startTurnFor(int team);
+
+/**
+ * Moves a unit to a particular tile
+ * @param unitID the unit to move
+ * @param x
+ * @param y
+ * @return bool true if the move was successful
+ */
+bool moveUnitTo(int unitID, int x, int y);
+
+/**
+ * Has a unit attack another unit
+ * @param unitID the unit to move
+ * @param targetUnitID the unit to attack
+ * @return bool true if attack was successful
+ */
+bool attackUnit(int unitID, int targetUnitID);
 
 #endif
--- source/battle.c
+++ source/battle.c
@@ -20,32 +20,113 @@ const struct Tile allTiles[12] = {
 
 const struct Unit allUnits[12] = {
     {"Y Ddraig Goch", TEAM_CYMRU, TYPE_AIR,
-     (int[3]){8, 6, 6}, true},
+     (int[3]){8, 6, 6}, true, 6},
 
     {"Barbary Lion", TEAM_ENGLAND, TYPE_LAND,
-     (int[3]){6, 6, 8}, true},
+     (int[3]){6, 6, 8}, true, 10},
 
     {"Loch Ness Monster", TEAM_SCOTLAND, TYPE_WATER,
-     (int[3]){6, 8, 6}, true},
+     (int[3]){6, 8, 6}, true, 10},
 
     {"Longboat", TEAM_SCOTLAND, TYPE_WATER,
-     (int[3]){4, 3, 4}, true}, // total 11
+     (int[3]){4, 3, 4}, true, 4}, // total 11
     {"Clansmen", TEAM_SCOTLAND, TYPE_LAND,
-     (int[3]){2, 3, 1}, true}, // total 6
+     (int[3]){2, 3, 1}, true, 5}, // total 6
     {"Teuchtars", TEAM_SCOTLAND, TYPE_AIR,
-     (int[3]){3, 2, 1}, true}, // total 6
+     (int[3]){3, 2, 1}, true, 2}, // total 6
 
     {"Mountaineers", TEAM_ENGLAND, TYPE_AIR,
-     (int[3]){3, 1, 2}, true}, // total 6
+     (int[3]){3, 1, 2}, true, 2}, // total 6
     {"Privateers", TEAM_ENGLAND, TYPE_WATER,
-     (int[3]){2, 2, 2}, true}, // total 6
+     (int[3]){2, 2, 2}, true, 4}, // total 6
     {"Cavalry", TEAM_ENGLAND, TYPE_LAND,
-     (int[3]){3, 3, 5}, true}, // total 11
+     (int[3]){3, 3, 5}, true, 5}, // total 11
 
     {"Rangers", TEAM_CYMRU, TYPE_AIR,
-     (int[3]){3, 5, 3}, true}, // total 11
+     (int[3]){3, 5, 3}, true, 2}, // total 11
     {"Fishermen", TEAM_CYMRU, TYPE_WATER,
-     (int[3]){1, 2, 3}, true}, // total 6
+     (int[3]){1, 2, 3}, true, 4}, // total 6
     {"Spearmen", TEAM_CYMRU, TYPE_LAND,
-     (int[3]){1, 3, 2}, true} // total 6
+     (int[3]){1, 3, 2}, true, 5} // total 6
 };
+
+void calculateVisibleTiles(int team)
+{
+    for (int i = 0; i < MAX_UNITS * 3; i++)
+    {
+        struct Unit u = allUnits[loadedUnits[i].type];
+        if (u.team == team)
+        {
+            int r = u.stats[BUFF_SIGHT];
+            while (r > 0)
+            {
+                int x = r;
+                int y = 0;
+                int d = 0;
+                while (x >= y)
+                {
+                    visibleMapTiles[(y * MAP_W) + x] = true;
+                    d += (2 * y + 1);
+                    if (d >= 0)
+                    {
+                        d += (-2 * x + 1);
+                        x--;
+                    }
+                }
+                r--;
+            }
+        }
+    }
+}
+
+void startTurnFor(int team)
+{
+    calculateVisibleTiles(team);
+    for (int i = 0; i < MAX_UNITS * 3; i++)
+    {
+        struct Unit u = allUnits[loadedUnits[i].type];
+        if (u.team == team)
+        {
+            loadedUnits[i].hasAttackedThisTurn = false;
+            loadedUnits[i].movement = u.stats[BUFF_SPEED];
+            loadedUnits[i].isVisibleThisTurn = true;
+        }
+        else
+        {
+            loadedUnits[i].hasAttackedThisTurn = true;
+            loadedUnits[i].movement = 0;
+            loadedUnits[i].isVisibleThisTurn = visibleMapTiles[loadedUnits[i].x + (loadedUnits[i].y * MAP_W)];
+        }
+    }
+}
+
+bool moveUnitTo(int unitID, int x, int y)
+{
+    int xDist = abs(x - loadedUnits[unitID].x);
+    int yDist = abs(y - loadedUnits[unitID].y);
+
+    if (xDist + yDist > loadedUnits[unitID].movement)
+    {
+        return false;
+    }
+    loadedUnits[unitID].x = x;
+    loadedUnits[unitID].y = y;
+    return true;
+}
+
+bool attackUnit(int unitID, int targetUnitID)
+{
+    struct MUnit curr = loadedUnits[unitID];
+    struct MUnit target = loadedUnits[targetUnitID];
+    struct Unit u = allUnits[curr.type];
+
+    int xDist = abs(curr.x - target.x);
+    int yDist = abs(curr.y - target.y);
+
+    int totalDistance = xDist + yDist;
+    if(totalDistance <= 1 || (totalDistance <= 2 && u.type == TYPE_WATER)){
+        target.health -= u.stats[BUFF_STRENGTH];
+        return true;
+    }
+    return false;
+}
