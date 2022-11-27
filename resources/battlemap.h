3883e9c0242b931e234b0f74ef5c476fd3f1e11d
Tania <90781871+woahtania@users.noreply.github.com>
2022-11-27 17:49:11 +0000

    Adds more tiles to battle map

--- resources/battlemap.h
+++ resources/battlemap.h
@@ -4,13 +4,12 @@
 //======================================================================
 //
 //	battlemap, 512x512@8, 
-//	Transparent color : FF,00,F6
 //	+ palette 256 entries, not compressed
-//	+ 19 tiles (t|f reduced) not compressed
+//	+ 39 tiles (t|f reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 64x64 
-//	Total size: 512 + 1216 + 8192 = 9920
+//	Total size: 512 + 2496 + 8192 = 11200
 //
-//	Time-stamp: 2022-11-26, 11:07:31
+//	Time-stamp: 2022-11-27, 17:48:03
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -19,8 +18,8 @@
 #ifndef GRIT_BATTLEMAP_H
 #define GRIT_BATTLEMAP_H
 
-#define battlemapTilesLen 1216
-extern const unsigned int battlemapTiles[304];
+#define battlemapTilesLen 2496
+extern const unsigned int battlemapTiles[624];
 
 #define battlemapMapLen 8192
 extern const unsigned short battlemapMap[4096];
